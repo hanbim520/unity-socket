3dbfe6e67ed98554f9c61b934a0ae36d993262fa
zhanghaijun03 <710605420@qq.com>
2018-03-19 19:16:49 +0800

    添加发消息样式

--- Test.cs
+++ Test.cs
@@ -6,11 +6,18 @@ using xClient.Config;
 using xClient.Core.Utilities;
 using xClient.Core.Helper;
 using System.Threading;
+using xClient.Core.Packets.ClientPackets;
+using xClient.Core.Cryptography;
 
 public class Test : MonoBehaviour {
     private static Thread thread;
+    private QuasarClient ConnectClient;
+    private Object thisLock = new Object();
+    private float lastTime;
+    private float curTime;
     // Use this for initialization
     void Start () {
+        AES.SetDefaultKey(Settings.KEY, Settings.AUTHKEY);
         thread = new Thread(Run);
         thread.Start();
     }
@@ -18,17 +25,36 @@ public class Test : MonoBehaviour {
     void Run()
     {
         var hosts = new HostsManager(HostHelper.GetHostsList(Settings.HOSTS));
-        QuasarClient ConnectClient = new QuasarClient(hosts);
+        ConnectClient = new QuasarClient(hosts);
         if (!QuasarClient.Exiting)
             ConnectClient.Connect();
       
     }
+
+    private void SendMsg()
+    {
+        new SetStatus("Updating...").Execute(ConnectClient);
+    }
     // Update is called once per frame
     void Update () {
-		
-	}
+        curTime = Time.time;
+        if (curTime - lastTime >= 3 && ConnectClient.Connected)
+        {
+            var sendThread = new Thread(SendMsg);
+            sendThread.Start();
+            lastTime = curTime;
+        }
+    }
     private void OnDestroy()
     {
+        if(!QuasarClient.Exiting)
+        {
+            lock (thisLock)
+            {
+                QuasarClient.Exiting = true;
+            }
+           
+        }
         thread.Abort();
         thread = null;
     }
